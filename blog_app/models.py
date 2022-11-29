cec1ca024a61561078f90d882493df4340cab214
Dennis Akagha <dennis.akagha@afripointgroup.com>
2022-11-29 02:31:10 +0100

    Add blog model and  comment model

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateField(default=date.today)
+    slug = models.CharField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name + "==>" + str(self.author)
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name + "-" + str(self.created))
+
+        return super().save(*args, **kwargs)
+
+class comment(models.Model):
+    body: models.TextField()
+    author: models.ForeignKey(User, on_delete=models.SET_NULL)
+    commment_date: models.DateTimeField(auto_now_add=True)
+    blog: models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.blog.title
+    
+
+    
